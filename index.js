9ac8e907a8359a4eb319352bf9bd9d97cc23834d
stacyvana <ssvana83@yahoo.com>
2022-07-11 11:23:26 -0400

    commit tree f03210051b3df935a9b318ddeb3d2fb3aa9d8c8e

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(x,y) {
+    let z = x/y;
+    return z;
+}
+
+const square = a => a * a;
+
+//const add = (a, b, c) => this.num + a + b + c;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
